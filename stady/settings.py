1c7b2a3fb376774746670627c6064b0e284083f0
egyeasy <dz1120@gmail.com>
2019-08-19 16:51:18 +0000

    bootstrap4 제거

--- stady/settings.py
+++ stady/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4',
+    # 'bootstrap4',
     'simple_email_confirmation',
     'django.contrib.admin',
     'django.contrib.auth',
